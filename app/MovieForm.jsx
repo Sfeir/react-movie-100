2c68d0dc9f7163001230b7a7b9f71c646d8794a5
ThomasCrvsr <crevoisier.thomas@gmail.com>
2016-06-01 16:49:49 +0200

    Ajout du composant MovieForm T_T

---
+++ app/MovieForm.jsx
@@ -0,0 +1,44 @@
+var React = require('react');
+
+var MovieForm = React.createClass({
+	render: function () {
+		return (
+			<div className="row movie-form">
+				<form className="col s12 card-panel">
+					<div className="row center-align">
+						<h5>Créer un nouveau film</h5>
+					</div>
+					<div className="row">
+						<div className="input-field col s12">
+							<input placeholder="Titre du film" type="text" className="validate"/>
+							<label className="active">Titre</label>
+						</div>
+					</div>
+					<div className="row">
+						<div className="input-field col s12">
+							{/* on pourrait utiliser un select */}
+							<input placeholder="Categorie du film" type="text" className="validate"/>
+							<label for="last_name" className="active">Categorie</label>
+						</div>
+					</div>
+					<div className="row">
+						<div className="input-field col s12">
+							<textarea className="materialize-textarea"></textarea>
+							<label className="active">Synopsis</label>
+						</div>
+					</div>
+
+					<div className="row right-align">
+						<div className="col s12">
+							<button className="btn waves-effect waves-light" type="submit" name="action">Submit
+								<i className="material-icons right">send</i>
+							</button>
+						</div>
+					</div>
+				</form>
+			</div>
+		);
+	}
+});
+
+module.exports = MovieForm;
