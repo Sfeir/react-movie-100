086018caa9e55a1b6bfed5490149a1ab1098bf1f
ThomasCrvsr <crevoisier.thomas@gmail.com>
2016-06-06 21:42:40 +0200

    Passage à materialize

---
+++ routes/data/movies.js
@@ -0,0 +1,25 @@
+exports.movies = [{
+    id: 1,
+    title: "Scott Pilgrim",
+    category: 'comedy',
+    poster: "img/scott_pilgrim.jpg",
+    synopsis: "Scott Pilgrim must defeat his new girlfriend's seven evil exes in order to win her heart."
+}, {
+    id: 2,
+    title: "Rick and Morty",
+    category: 'adventure',
+    poster: "img/rick_morty.jpg",
+    synopsis: "An animated series that follows the exploits of a super scientist and his not so bright grandson."
+}, {
+    id: 3,
+    title: "Silicon Valley",
+    category: 'comedy',
+    poster: "img/silicon_valley.jpg",
+    synopsis: "In the high-tech gold rush of modern Silicon Valley, the people most qualified to succeed are the least capable of handling success. A comedy partially inspired by Mike Judge's own experiences as a Silicon Valley engineer in the late 1980s"
+}, {
+    id: 4,
+    title: "Kung Fury",
+    category: 'action',
+    poster: "img/kung_fury.jpg",
+    synopsis: "In 1985, Kung Fury, the toughest martial artist cop in Miami, goes back in time to kill the worst criminal of all time - kung fuhrer Hitler."
+}];
