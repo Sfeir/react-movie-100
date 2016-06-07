e29ddd81c2c9ac1a1a0915c589cfd611ace9840a
ThomasCrvsr <crevoisier.thomas@gmail.com>
2016-06-07 20:35:44 +0200

    Mise à disposition d'une façade pour le serveur

---
+++ app/api/MovieAPI.js
@@ -0,0 +1,27 @@
+'use strict';
+
+var axios = require('axios');
+
+function fetchMovies() {
+	return axios.get('/server/api/movies')
+		.then(function (response) {
+			return response.data;
+		});
+}
+
+function removeMovie(id) {
+	return axios.delete('/server/api/movies/' + id);
+}
+
+function addMovie(movie) {
+	return axios.post('/server/api/movies/', movie)
+		.then(function (response) {
+			return response.data;
+		});
+}
+
+module.exports = {
+	fetchMovies: fetchMovies,
+	removeMovie: removeMovie,
+	addMovie: addMovie
+};
